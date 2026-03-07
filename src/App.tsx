6bfcc6493377a10af3917a521638fe0ef10d5ff1
PrideMarket <auto@pridemarket.com>
2026-03-07 20:18:13 -0300

    feat-vercel-speed-insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import Layout from './components/Layout';
 import MarketHome from './pages/MarketHome';
 import ItemDetail from './pages/ItemDetail';
@@ -6,15 +7,18 @@ import Analytics from './pages/Analytics';
 
 export default function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<MarketHome />} />
-                    <Route path="item/:name" element={<ItemDetail />} />
-                    <Route path="analytics" element={<Analytics />} />
-                </Route>
-                <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
-        </BrowserRouter>
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<MarketHome />} />
+                        <Route path="item/:name" element={<ItemDetail />} />
+                        <Route path="analytics" element={<Analytics />} />
+                    </Route>
+                    <Route path="*" element={<Navigate to="/" replace />} />
+                </Routes>
+            </BrowserRouter>
+            <SpeedInsights />
+        </>
     );
 }
